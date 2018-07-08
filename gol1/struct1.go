2224845a9f9a699fc83251f7e951b1207041801a
Menino <meninoborges@hotmail.com>
2018-07-08 08:41:20 +0100

    Go Routes 1

--- gol1/struct1.go
+++ gol1/struct1.go
@@ -2,13 +2,19 @@ package main
 
 import "fmt"
 
+const usixteenbitmax float64 = 65535
+const kmh_multiple float64 = 1.60934
+
 type car struct {
-	gas_pedal unint16 //min 0 max 65535
-	brake_pedal unint16
+	gas_pedal uint16 //min 0 max 65535
+	brake_pedal uint16
 	steering_wheel int16 //  -32k - +32k
 	top_speed_kmh float64
 }
 
+func (c car) kmh() float64 {
+	return  float64(c.gas_pedal) * (c.top_speed_kmh/usixteenbitmax)
+}
 func main() {
 	a_car := car{gas_pedal: 22341, 
 	brake_pedal: 0, 
@@ -16,7 +22,7 @@ func main() {
 	top_speed_kmh: 225.0}
 	
 	fmt.Println(a_car.gas_pedal)
-
+	fmt.Println(a_car.kmh())
 
 
 
